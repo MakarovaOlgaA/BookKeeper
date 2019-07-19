8bae8e11221170b82bf9065a32f7f909bb42ffe9
MakarovaOlgaA <35731398+MakarovaOlgaA@users.noreply.github.com>
2019-07-19 17:28:14 +0300

    added UserRuleHandler

--- BookKeeper/PaymentCalculations/UserRuleHandler.cs
+++ BookKeeper/PaymentCalculations/UserRuleHandler.cs
@@ -1,6 +1,30 @@
 ﻿namespace BookKeeper.PaymentCalculations
 {
+    using BookKeeper.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UserDataAPI.Models;
+
     public class UserRuleHandler: PaymentHandlerBase
     {
+        public override decimal Calculate(UserResultVM user, UserResult initialUserResult, DateTime startDate, DateTime endDate)
+        {
+            var baseRate = 0M;
+
+            var userRuleIds = initialUserResult.UserRules.Where(ur => ur.UserId == user.Id).Select(ur => ur.RuleId);
+            var rules = initialUserResult.Rules.Where(r => userRuleIds.Contains(r.Id));
+
+            foreach (var rule in rules)
+            {
+                var dateFrom = rule.StartDate > startDate ? rule.StartDate : startDate;
+                var dateTo = rule.EndDate.HasValue && rule.EndDate < endDate ? rule.EndDate.Value : endDate;
+
+                var calculator = CalculatorFactory.GetRuleCalculator(user, initialUserResult, rule.RuleTypeId);
+                baseRate += calculator.Calculate(rule.Bonus, dateFrom, dateTo);
+            }
+
+            return baseRate + Proceed(user, initialUserResult, startDate, endDate);
+        }
     }
 }
